a1065c78d0f53e282750fc8a338f97a8155b5b3b
amdee <amdee@yahoo.com>
2024-07-10 16:09:54 +0530

    Revert DSA prep commit
    This reverts commit 54565f521b1003da71182b7c4ad587a2199e9ad1.

--- Day3.java
+++
@@ -1,9 +0,0 @@
-public class Day3 {
-    public static void main(String[] args) {
-        System.out.println(2&3);
-        System.out.println(5|10);
-        System.out.println(~1);
-        System.out.println(4<<1); // multipled by 2 twice itself when you left shift 2*n
-        System.out.println(5>>1); // divided by 2power1 or so on ....  / 2^n
-    }
-}
